50d2730110d36a75b86612328f976a930d9e44a7
Marcin Kołodziej <mar.kolodziej@icloud.com>
2024-12-12 20:27:43 +0100

    feat: update after lesson 5

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,68 @@
 import { useRouter } from 'expo-router';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, View, Text, Button, FlatList } from 'react-native';
+import axios from 'axios';
+
+export type TUser = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    }
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  }
+}
 
 export default function HomeScreen() {
   const router = useRouter();
+  const [users, setUsers] = useState<TUser[]>([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response => setUsers(response.data))
+      .catch(error => console.error(error));
+  }, []);
 
   return <View style={styles.container}>
-    <Text style={styles.title}>Ekran główny</Text>
-    <Button title={'Idź do About'} onPress={() => router.push('/about')}/>
-    <Button title={'Idź do Details nr 42'} onPress={() => router.push('/details/42')}/>
+    <Text style={styles.title}>Lista użytkowników</Text>
+    <FlatList
+      data={users}
+      keyExtractor={user => user.id.toString()}
+      renderItem={({ item }) => <View style={styles.userItem}>
+          <Button title={item.name} onPress={() => router.push(`/details/${item.id}`)} />
+        </View>}
+    />
   </View>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f0f0f0',
+    paddingTop: 80,
   },
   title: {
+    flex: 1,
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 20
+    textAlign: 'center'
+  },
+  userItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc'
   }
 });
--- app/details/[id].tsx
+++ app/details/[id].tsx
@@ -1,11 +1,37 @@
 import { useLocalSearchParams, useRouter } from 'expo-router';
+import { useEffect, useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import axios from 'axios';
+import { TUser } from '../(tabs)';
 
 export default function DetailsScreen() {
   const { id } = useLocalSearchParams();
+  const [user, setUser] = useState<TUser | null>(null);
+
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then(response => setUser(response.data))
+      .catch(error => console.error(error));
+  }, [])
   
   return <View style={styles.container}>
-    <Text style={styles.title}>Ekran szczegółów nr {id}</Text>
+    <Text style={styles.title}>Użytkownik nr {id}</Text>
+    {user ? <View>
+        <Text>{user.name}</Text>
+        <Text>{user.username}</Text>
+        <Text>{user.email}</Text>
+        <Text>{user.phone}</Text>
+        <Text>{user.website}</Text>
+        <Text>{user.address.street}</Text>
+        <Text>{user.address.suite}</Text>
+        <Text>{user.address.city}</Text>
+        <Text>{user.address.zipcode}</Text>
+        <Text>{user.address.geo.lat}</Text>
+        <Text>{user.address.geo.lng}</Text>
+        <Text>{user.company.name}</Text>
+        <Text>{user.company.catchPhrase}</Text>
+        <Text>{user.company.bs}</Text>
+      </View> : <Text>Ładowanie...</Text>}
   </View>;
 }
 
