95204737e312eb29f0d1715dceec40b59634e5b6
MarekLempart <144551338+MarekLempart@users.noreply.github.com>
2025-02-07 15:31:35 +0100

    Feature/d l mode (#2)
    * Create README_GitFlow.md
    
    * theme fix

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -1,10 +1,26 @@
 // components/ThemeSwitcher.tsx
-import React, { useContext } from "react";
+// import React, { useContext } from "react";
+// import { Button, View } from "react-native";
+// import { ThemeContext } from "@/contexts/ThemeContext";
+
+// const ThemeSwitcher = (): JSX.Element => {
+//   const { toggleTheme } = useContext(ThemeContext);
+//   return (
+//     <View style={{ margin: 10 }}>
+//       <Button title="Toggle Theme" onPress={toggleTheme} />
+//     </View>
+//   );
+// };
+
+// export default ThemeSwitcher;
+
+// components/ThemeSwitcher.tsx
+import React from "react";
 import { Button, View } from "react-native";
-import { ThemeContext } from "@/contexts/ThemeContext";
+import { useTheme } from "@/contexts/ThemeContext";
 
 const ThemeSwitcher = (): JSX.Element => {
-  const { toggleTheme } = useContext(ThemeContext);
+  const { toggleTheme } = useTheme();
   return (
     <View style={{ margin: 10 }}>
       <Button title="Toggle Theme" onPress={toggleTheme} />
--- components/ui/TabBarBackground.tsx
+++ components/ui/TabBarBackground.tsx
@@ -1,5 +1,19 @@
-// This is a shim for web and Android where the tab bar is generally opaque.
-export default undefined;
+// // This is a shim for web and Android where the tab bar is generally opaque.
+// export default undefined;
+
+// export function useBottomTabOverflow() {
+//   return 0;
+// }
+
+import { View, ViewStyle } from "react-native";
+
+interface TabBarBackgroundProps {
+  style?: ViewStyle; // Obsługa stylów
+}
+
+export default function TabBarBackground({ style }: TabBarBackgroundProps) {
+  return <View style={[{ backgroundColor: "transparent" }, style]} />;
+}
 
 export function useBottomTabOverflow() {
   return 0;
