abab3980065c4b87cd80d71141f6987ce138a3ab
MarekLempart <kama.zakupy@gmail.com>
2025-02-13 08:25:19 +0100

    Details fix

--- app/details/detail.tsx
+++ app/details/detail.tsx
@@ -1,19 +1,35 @@
 // app/details/detail.tsx
-import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
-import { useRouter } from "expo-router";
+import React, { useLayoutEffect} from "react";
+import { View, Text, StyleSheet, Button, Platform } from "react-native";
+import { useRouter, useNavigation } from "expo-router";
 import { useTheme } from "@/contexts/ThemeContext";
 
 const DetailScreen = (): JSX.Element => {
     const router = useRouter();
+    const navigation = useNavigation();
     const { theme } = useTheme();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerStyle: { backgroundColor: theme.colors.bgSecondary },
+            headerTitleStyle: { color: theme.colors.textSecondary },
+            headerTintColor: theme.colors.accent,
+            ...(Platform.OS === "ios" && {
+                headerBackTitle: "Cards",
+                headerBackTitleStyle: { color: theme.colors.accent },
+            }),
+        });
+    }, [navigation, theme]);
+
     const handleDetails = (): void => {
         // Tymczasowe przekierowanie
         router.push("/(tabs)");
     };
     return (
         <View style={[styles.container, { backgroundColor: theme.colors.bgPrimary }]}>
-            <Text style={[styles.title, { color: theme.colors.textPrimary }]}>Resource details</Text>
+            <Text style={[styles.title, { color: theme.colors.textPrimary }]}>
+                Resource details
+            </Text>
             <View style={styles.buttonContainer}>
                 <Button title="Details" onPress={handleDetails} />
                 <Button title="Back" onPress={() => router.back()} />
